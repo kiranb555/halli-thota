602d2ec736154ecbf70674765d801fa47e69f7c5
kiranb555 <kiran.basavaraj05@gmail.com>
2026-02-22 01:38:01 +0530

    build again

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/halli-thota',
+  assetPrefix: '/halli-thota',
+  trailingSlash: true,
 };
 
 export default nextConfig;
