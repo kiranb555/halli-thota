c7652a5679cb255d887aba1f399a74902566ade7
kiranb555 <kiran.basavaraj05@gmail.com>
2026-02-22 11:18:23 +0530

    added marquee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,10 @@ import { Fragment } from 'react/jsx-runtime';
 function Home() {
   return (
       <main className="mt-5 mb-5 mx-5">
-        <div className="marquee w-full">
-          {[1,2,3,4,5].map((item) => (
-            <Fragment key={item}>
+            <marquee>
               <span className="text-red-500 pr-3 animate-ping pl-5">Sale</span>
               <span className="text-green-500 animate-pulse">ಈ ವೆಬ್‌ಸೈಟ್ ಮಾರಾಟಕ್ಕಿದೆ ಸಂಪರ್ಕಿಸಿ - <span className="text-black animate-pulse">9741621201</span></span>
-            </Fragment>
-          ))} 
-        </div>
+            </marquee>
         <h1 className="md:text-9xl text-5xl font-bold text-center mt-10">
           <span  className="text-pink-500 pr-3">Halli</span>
           <span className="text-green-500">Thota</span>
